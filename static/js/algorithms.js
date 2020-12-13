6ac69b1d46408e351500a012aa232404149e6c6a
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-12-13 13:22:14 -0800

    add bubble sort

--- static/js/algorithms.js
+++ static/js/algorithms.js
@@ -10,6 +10,7 @@ var svg = d3.select("svg")
               .attr("height", height);
 var array = [];
 n = 100;
+var barWidth = width / n - barPadding;
 
 // --------------------------------------
 // Global Functions
@@ -24,21 +25,11 @@ for (var i = 0; i < n; i++){
 }
 console.log(array);
 
-function newData() {
-  array = [];
-  // Create random number array
-  for (var i = 0; i < n; i++){
-    array.push(getRandomInt(1000));
-  }
-  console.log(array);
-}
-
 // --------------------------------------
-// Draw graphs
+// Draw Graph
 // --------------------------------------
 
-// Calculate width of individual bars
-var barWidth = width / n - barPadding;
+function drawGraph(array) {
 
 // Create y-axis scale to ensure bars fit inside svg area and for calculating y and height attribute in rect
 var yScale = d3.scaleLinear()
@@ -50,23 +41,66 @@ var yScale = d3.scaleLinear()
 // }
 
 // Create bars variable
-var bars = d3.select("#linear-search")
+var bars = d3.select("svg")
                 .attr("width", width)
                 .attr("height", height)
               .selectAll(".bar")
               .data(array)
-              .enter()
-              .append("g")
-                .classed("bar", true)
-console.log("bars: ", bars);
+
+var g = bars
+          .enter()
+          .append("g")
+            .classed("bar", true)
+
+bars
+  .exit()
+  .remove();
 
 // Draw rectangles
-bars
-  .append("rect")
-    .attr("x", (d, i) => {return (barWidth + barPadding) * i})
-    .attr("y", (d) => {return height - yScale(d)})
-    .attr("height", (d) => {return yScale(d)})
-    .attr("width", (d) => barWidth);
+g.append("rect")
+  .attr("x", (d, i) => {return (barWidth + barPadding) * i})
+  .attr("y", (d) => {return yScale(d)})
+  .attr("height", (d) => {return height - yScale(d)})
+  .attr("width", (d) => barWidth);
+
+g.merge(bars)
+  .select("rect")
+  .attr("x", (d, i) => {return (barWidth + barPadding) * i})
+  .attr("y", (d) => {return yScale(d)})
+  .attr("height", (d) => {return height - yScale(d)})
+  .attr("width", (d) => barWidth);
+}
+
+d3.select(window).on("load", drawGraph(array));
+
+// --------------------------------------
+// Draw New Graph
+// --------------------------------------
+
+// d3.select("#new-data").on("click", () => {
+
+//   newArray = [];
+//   // Create random number array
+//   for (var i = 0; i < n; i++){
+//     newArray.push(getRandomInt(1000));
+//   }
+//   console.log(newArray);
+
+//   d3.select("svg")
+//     .selectAll(".bar")
+//     .data(newArray)
+//     .enter()
+//     .append("g")
+//       classed("bar", true)
+//     .append("rect")
+//       .attr("x", (d, i) => {return (barWidth + barPadding) * i})
+//       .attr("y", (d) => {return height - yScale(d)})
+//       .attr("height", (d) => {return yScale(d)})
+//       .attr("width", (d) => barWidth);
+
+//   // General Update Pattern: remove old data
+//   d3.select("new-data").remove();
+// });
 
 
 // --------------------------------------
@@ -105,7 +139,7 @@ d3.select("form").on("submit", () => {
   // End run-time stopwatch
   var end = window.performance.now();
   d3.select("body").append("div")
-    .text(`Execution time: ${end - start} ms`);
+    .text(`Linear Search Execution time: ${end - start} ms`);
 
 });
 
@@ -120,7 +154,63 @@ d3.select("form").on("submit", () => {
 // Bubble Sort
 // --------------------------------------
 
+d3.select("#bubble-sort").on("click", () => {
+  // Prevent page from reloading
+  d3.event.preventDefault();
 
+  var yScale = d3.scaleLinear()
+                  .domain([0, Math.max(...array)])
+                  .range([height, 0]);
+
+  var bars = d3.select("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .selectAll(".bar")
+                .data(array)
+
+  var g = bars
+    .enter()
+    .append("g")
+      .classed("bar", true)
+
+  bars
+  .exit()
+  .remove();
+
+  g.merge(bars)
+    .select("rect")
+    .attr("x", (d, i) => {return (barWidth + barPadding) * i})
+    .attr("y", (d) => {return yScale(d)})
+    .attr("height", (d) => {return height - yScale(d)})
+    .attr("width", (d) => barWidth);
+
+  // Begin run-time stopwatch
+  var start = window.performance.now();
+
+  let len = array.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < len; i++) {
+      if (array[i] > array[i + 1]) {
+        let first = array[i];
+        let second = array[i + 1];
+        array[i] = second;
+        array[i + 1] = first;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  console.log(array);
+
+  // End run-time stopwatch
+  var end = window.performance.now();
+  d3.select("body").append("div")
+    .text(`Bubble Sort Execution time: ${end - start} ms`);
+  
+    drawGraph(array)
+  return array;
+});
 
 // --------------------------------------
 // Selection Sort
