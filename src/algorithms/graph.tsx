7638e128053349f71bc8d8595269e4f6860d19be
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-29 00:20:53 -0700

    fix tooltip after D3 relased new version. add if statement to toggle animation on unsort and newData functions

--- src/algorithms/graph.tsx
+++ src/algorithms/graph.tsx
@@ -4,11 +4,9 @@ import { n } from './global';
 const width: number = 600;
 const height: number = 600;
 const barPadding: number = 1;
-const svg: any = d3.select("svg")
-                .attr("viewBox", `0 0 ${width} ${height}`)
 var barWidth: number = width / n - barPadding;
 
-export const drawGraph = (array : number[]) => {
+export const drawGraph = (array : number[], reset: boolean) => {
 
   // Create y-axis scale to ensure bars fit inside svg area and for calculating y and height attribute in rect
   var yScale: any = d3.scaleLinear()
@@ -31,36 +29,54 @@ export const drawGraph = (array : number[]) => {
   // Create a tooltip
   var tooltip: any = d3.select("#tooltip")
                        .append("div")
-                       .style("opacity", 0)
                        .attr("class", "tooltip");
 
   // Draw rectangles
   g.append("rect")
-    .attr("transform", function(d: any, i: any) {return `translate(${(xScale(i) - (barWidth + barPadding))},0)`;})
+    .attr("transform", function(d: any, i: number) {return `translate(${(xScale(i) - (barWidth + barPadding))},0)`;})
     .attr("y", (d: any) => {return yScale(d);})
     .attr("height", (d: any) => {return height - yScale(d);})
     .attr("width", (d: any) => barWidth)
-    // .on("mousemove", (d: any, i: any) => {tooltip.transition().duration(100).style("opacity", 0.9);
-    // tooltip.html(event, d).style("left", (event.pageX) + "px").style("top", (event.pageY) + "px");})
-    // .on("mouseleave", (d: any) => {tooltip.transition().duration(300).style("opacity", 0);});
-
-    // .attr("x", (d, i) => {return (barWidth + barPadding) * i})
+    .on("mouseover", function(event: any, d: any) {
+      const[x, y] = d3.pointer(event, bars.node());
+      tooltip.html(d)
+              .style("left", `${x}px`)
+              .style("top", `${y}px`)
+              .style("opacity", 0.9)
+              .transition()
+              .duration(100);
+    })
+  .on("mouseleave", (d: any) => {
+    d3.select(".tooltip")
+      .transition()
+      .duration(300)
+      .style("opacity", 0);
+  });
 
   // Remove old data
   bars.exit()
       .remove();
 
   // Update data
-  g.merge(bars)
+  if (reset === false) {
+    // Standard behavior
+    g.merge(bars)
     .select("rect")
-    .attr("transform", function(d: any, i: any) {return `translate(${(xScale(i) - (barWidth + barPadding))},0)`;})
+    .attr("transform", function(d: any, i: number) {return `translate(${(xScale(i) - (barWidth + barPadding))},0)`;})
     .attr("y", (d: any) => {return yScale(d);})
     .attr("height", (d: any) => {return height - yScale(d);})
     .attr("width", (d: any) => barWidth)
-    .attr("id", function(d: any, i: any) {return `bar_${i}`;});
-
-    // Pleasant animation
-    // Add this to g.merge(bars) when clicking on Unsort or New Data
-    // .transition()
-    // .delay(function(d, i) { return i * 5; })
+    .attr("id", function(d: any, i: number) {return `bar_${i}`;});
+  } else {
+    // Pleasant step-wise animation
+    g.merge(bars)
+    .select("rect")
+    .transition()
+    .delay(function(d: any, i: number) { return i * 5; })
+    .attr("transform", function(d: any, i: number) {return `translate(${(xScale(i) - (barWidth + barPadding))},0)`;})
+    .attr("y", (d: any) => {return yScale(d);})
+    .attr("height", (d: any) => {return height - yScale(d);})
+    .attr("width", (d: any) => barWidth)
+    .attr("id", function(d: any, i: number) {return `bar_${i}`;});
+  }
 }
